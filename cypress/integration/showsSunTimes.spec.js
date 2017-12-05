b4115d924eaf1e72367eb09b778253c2650ecc37
nwshane <django09@gmail.com>
2017-12-05 18:23:59 -0500

    Stubbed getCurrentPosition to speed up integration spec for showing sunset and sunrise times.

--- cypress/integration/showsSunTimes.spec.js
+++ cypress/integration/showsSunTimes.spec.js
@@ -1,16 +1,37 @@
+const position = {
+  coords: {
+    latitude: '40.6396951',
+    longitude: '-73.96767679999999'
+  }
+}
+
+const sunResponse = {
+  results: {
+    sunrise: '9:13:12 AM',
+    sunset: '7:15:16 PM'
+  }
+}
+
 describe('Home page', () => {
   it('shows sunrise and sunset times', () => {
+    const { latitude, longitude } = position.coords
+
     cy.server()
     cy
-      .route('https://api.sunrise-sunset.org/json**', {
-        results: {
-          sunrise: '9:13:12 AM',
-          sunset: '7:15:16 PM'
-        }
-      })
+      .route(
+        `https://api.sunrise-sunset.org/json?lat=${latitude}&lng=${longitude}`,
+        sunResponse
+      )
       .as('getSunData')
 
-    cy.visit('http://localhost:3000')
+    cy.visit('http://localhost:3000', {
+      onBeforeLoad(pageWindow) {
+        // stubbing getCurrentPosition to speed it up
+        cy
+          .stub(pageWindow.navigator.geolocation, 'getCurrentPosition')
+          .callsArgWith(0, position)
+      }
+    })
 
     cy.wait('@getSunData')
     cy.contains('Sunrise: 9:13 am')
