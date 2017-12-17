cc6655d7559ccba822cf85e3beaaf12ccd0568ed
nwshane <django09@gmail.com>
2017-12-17 17:02:37 -0500

    Added triangle to end of hour hand.

--- components/SunClockCircle/SunClockCirclePresentation.js
+++ components/SunClockCircle/SunClockCirclePresentation.js
@@ -3,12 +3,33 @@ import NighttimeArc from './NighttimeArc'
 import HourMarkers from './HourMarkers'
 import HourHand from './HourHand'
 
+class TriangleMarker extends React.Component {
+  render() {
+    return (
+      <marker
+        id="triangle"
+        viewBox="0 0 10 10"
+        refX="1"
+        refY="5"
+        markerWidth="6"
+        markerHeight="6"
+        orient="auto"
+      >
+        <path d="M 0 0 L 10 5 L 0 10 z" />
+      </marker>
+    )
+  }
+}
+
 class SunClockCirclePresentation extends React.Component {
   render() {
     const { dimension } = this.props
 
     return (
       <svg width={`${dimension}px`} height={`${dimension}px`}>
+        <defs>
+          <TriangleMarker />
+        </defs>
         <DaylightArc {...this.props} />
         <NighttimeArc {...this.props} />
         <HourHand {...this.props} />
--- data/hourHandGetters.js
+++ data/hourHandGetters.js
@@ -7,10 +7,10 @@ import {
 } from './getters'
 
 const getHourHandXModifier = ({ dimension, time }) =>
-  getHorizontalAspectOfTime(time) * getClockInnerRadius(dimension)
+  getHorizontalAspectOfTime(time) * getClockInnerRadius(dimension) * 0.9
 
 const getHourHandYModifier = ({ dimension, time }) =>
-  getVerticalAspectOfTime(time) * getClockInnerRadius(dimension)
+  getVerticalAspectOfTime(time) * getClockInnerRadius(dimension) * 0.9
 
 export const getHourHandX1 = ({ dimension }) => getCenterX(dimension)
 export const getHourHandY1 = ({ dimension }) => getCenterY(dimension)
