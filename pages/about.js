ffe55d1d1670ff59af5ce3cee34f42736d22a4b6
nwshane <django09@gmail.com>
2017-12-26 20:25:45 -0500

    Added about page with credit for sun icon.

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+class AboutPage extends React.Component {
+  render() {
+    return (
+      <main>
+        <p>Sun icon credit: jeff from the Noun Project</p>
+      </main>
+    )
+  }
+}
+
+export default AboutPage
