3a09cb5fe0a48e4f0f6042a98211d4ed9f30352d
nwshane <django09@gmail.com>
2018-01-14 15:40:20 -0500

    WIP

--- components/SunClockCircle/index.stories.js
+++ components/SunClockCircle/index.stories.js
@@ -19,26 +19,38 @@ storiesOf('SunClockCircle', module)
     </Provider>
   ))
   .add('with long nighttime hours', () => (
-    <SunClockCircle
-      sunriseLocalTime={LocalTime.of(9, 32, 1)}
-      sunsetLocalTime={LocalTime.of(17, 5, 20)}
-      dimension={500}
-      clockDate={new Date(1970, 1, 1, 3, 2, 1)}
-    />
+    <Provider
+      store={createStore({
+        sunriseLocalTime: LocalTime.of(9, 32, 1),
+        sunsetLocalTime: LocalTime.of(17, 5, 20),
+        dimension: 500,
+        clockDate: new Date(1970, 1, 1, 3, 2, 1)
+      })}
+    >
+      <SunClockCircle />
+    </Provider>
   ))
   .add('with large dimensions and rising sun', () => (
-    <SunClockCircle
-      sunriseLocalTime={LocalTime.of(6, 55, 1)}
-      sunsetLocalTime={LocalTime.of(15, 20, 38)}
-      dimension={1200}
-      clockDate={new Date(1970, 1, 1, 7, 2, 1)}
-    />
+    <Provider
+      store={createStore({
+        sunriseLocalTime: LocalTime.of(6, 55, 1),
+        sunsetLocalTime: LocalTime.of(15, 20, 38),
+        dimension: 1200,
+        clockDate: new Date(1970, 1, 1, 7, 2, 1)
+      })}
+    >
+      <SunClockCircle />
+    </Provider>
   ))
   .add('with small dimensions and setting sun', () => (
-    <SunClockCircle
-      sunriseLocalTime={LocalTime.of(2, 47, 1)}
-      sunsetLocalTime={LocalTime.of(16, 40, 38)}
-      dimension={300}
-      clockDate={new Date(1970, 1, 1, 16, 35, 1)}
-    />
+    <Provider
+      store={createStore({
+        sunriseLocalTime: LocalTime.of(2, 47, 1),
+        sunsetLocalTime: LocalTime.of(16, 40, 38),
+        dimension: 300,
+        clockDate: new Date(1970, 1, 1, 16, 35, 1)
+      })}
+    >
+      <SunClockCircle />
+    </Provider>
   ))
