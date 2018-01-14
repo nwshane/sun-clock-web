36a9e3c02b665075df951f21600ea07253217ed1
nwshane <django09@gmail.com>
2018-01-14 15:33:24 -0500

    WIP

--- components/SunriseTime/index.stories.js
+++ components/SunriseTime/index.stories.js
@@ -1,7 +1,12 @@
-import SunriseTime from './'
 import { storiesOf } from '@storybook/react'
 import { LocalTime } from 'js-joda'
+import { Provider } from 'react-redux'
+
+import SunriseTime from './'
+import createStore from '../../data/createStore'
 
 storiesOf('SunriseTime', module).add('with early morning time', () => (
-  <SunriseTime sunriseLocalTime={LocalTime.of(2, 30, 23)} />
+  <Provider store={createStore({ sunriseLocalTime: LocalTime.of(2, 30, 23) })}>
+    <SunriseTime />
+  </Provider>
 ))
--- data/createStore.js
+++ data/createStore.js
@@ -1,12 +1,19 @@
 import Store, { thunk } from 'repatch'
 
-export default () =>
-  new Store({
-    latitude: null,
-    longitude: null,
-    clockDate: new Date(),
-    sunriseLocalTime: null,
-    sunsetLocalTime: null,
-    loading: true,
-    error: null
-  }).addMiddleware(thunk)
+export default (state = {}) =>
+  new Store(
+    Object.assign(
+      {},
+      {
+        latitude: null,
+        longitude: null,
+        clockDate: new Date(),
+        sunriseLocalTime: null,
+        sunsetLocalTime: null,
+        loading: true,
+        error: null,
+        interval: null
+      },
+      state
+    )
+  ).addMiddleware(thunk)
