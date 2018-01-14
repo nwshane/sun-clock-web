0b60ec53dc81c0aa82ea2918e988c885ddb7e8ee
nwshane <django09@gmail.com>
2018-01-14 15:34:55 -0500

    wip

--- components/SunsetTime/index.stories.js
+++ components/SunsetTime/index.stories.js
@@ -1,7 +1,12 @@
-import SunsetTime from './'
 import { storiesOf } from '@storybook/react'
 import { LocalTime } from 'js-joda'
+import { Provider } from 'react-redux'
+
+import SunsetTime from './'
+import createStore from '../../data/createStore'
 
 storiesOf('SunsetTime', module).add('with late night time', () => (
-  <SunsetTime sunsetLocalTime={LocalTime.of(23, 3, 57)} />
+  <Provider store={createStore({ sunsetLocalTime: LocalTime.of(23, 3, 57) })}>
+    <SunsetTime />
+  </Provider>
 ))
