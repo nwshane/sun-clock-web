53575087cc4b2b8a3b13dd2d2798fb91442faacb
nwshane <django09@gmail.com>
2018-02-19 17:26:50 -0500

    Updated SunClockCircle snapshots.

--- components/SunClockCircle/index.stories.js
+++ components/SunClockCircle/index.stories.js
@@ -6,48 +6,44 @@ import SunClockCircle from './'
 import createStore from '../../data/createStore'
 
 storiesOf('SunClockCircle', module)
-  .add('with long daylight hours', () => (
+  .add('at 2:02 pm with long daylight hours', () => (
     <Provider
       store={createStore({
         sunriseDate: new Date(1970, 1, 1, 3, 8, 1),
-        sunsetDate: new Date(1970, 1, 1, 3, 8, 1),
-        dimension: 500,
+        sunsetDate: new Date(1970, 1, 1, 20, 55, 20),
         clockDate: new Date(1970, 1, 1, 14, 2, 1)
       })}
     >
       <SunClockCircle />
     </Provider>
   ))
-  .add('with long nighttime hours', () => (
+  .add('at 3:02 am with long nighttime hours', () => (
     <Provider
       store={createStore({
         sunriseDate: new Date(1970, 1, 1, 9, 32, 1),
         sunsetDate: new Date(1970, 1, 1, 17, 5, 20),
-        dimension: 500,
         clockDate: new Date(1970, 1, 1, 3, 2, 1)
       })}
     >
       <SunClockCircle />
     </Provider>
   ))
-  .add('with large dimensions and rising sun', () => (
+  .add('at 7:02 am with rising sun', () => (
     <Provider
       store={createStore({
         sunriseDate: new Date(1970, 1, 1, 6, 55, 1),
         sunsetDate: new Date(1970, 1, 1, 15, 20, 38),
-        dimension: 1200,
         clockDate: new Date(1970, 1, 1, 7, 2, 1)
       })}
     >
       <SunClockCircle />
     </Provider>
   ))
-  .add('with small dimensions and setting sun', () => (
+  .add('at 4:35 pm with setting sun', () => (
     <Provider
       store={createStore({
         sunriseDate: new Date(1970, 1, 1, 2, 47, 1),
         sunsetDate: new Date(1970, 1, 1, 16, 40, 38),
-        dimension: 300,
         clockDate: new Date(1970, 1, 1, 16, 35, 1)
       })}
     >
