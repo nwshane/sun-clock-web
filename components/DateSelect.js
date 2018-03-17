151a5d65097df27e0292241353cb95f98c10b2ff
nwshane <django09@gmail.com>
2018-03-16 23:12:42 -0400

    Added Edit Me button for calendar, to be replaced by edit icon.

--- components/DateSelect.js
+++ components/DateSelect.js
@@ -16,26 +16,38 @@ class DateSelect extends React.Component {
   render() {
     return (
       <div data-name-for-tests="clock-date-select-container">
-        <label htmlFor="clock-date-picker">Clock Date</label>
-        <DatePicker
-          id="clock-date-picker"
-          name="clock-date-picker"
-          dateFormat="YYYY-MM-DD"
-          selected={moment(this.props.clockDate)}
-          onChange={this.handleChange}
-        />
+        <label htmlFor="clock-date-picker">
+          <span className="label-date">Date:</span>
+          <DatePicker
+            id="clock-date-picker"
+            name="clock-date-picker"
+            dateFormat="YYYY-MM-DD"
+            selected={moment(this.props.clockDate)}
+            onChange={this.handleChange}
+          />
+          <span className="label-edit-icon">Edit Me</span>
+        </label>
         <style jsx>{`
           div {
             position: absolute;
             bottom: 20px;
             left: 20px;
           }
+          label {
+            cursor: pointer;
+            display: flex;
+          }
+          .label-date {
+            margin-right: 7px;
+          }
+          .label-edit-icon {
+          }
         `}</style>
         <style jsx global>{`
           input#clock-date-picker {
             font-size: inherit;
             font-family: inherit;
-            max-width: 6em;
+            max-width: 5em;
             border: none;
             cursor: pointer;
           }
