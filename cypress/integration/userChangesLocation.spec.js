114700976895c5c0ece1fe347e07d53da1964dfa
nwshane <django09@gmail.com>
2018-04-14 16:03:52 -0400

    Simplified user changes location test.

--- cypress/integration/userChangesLocation.spec.js
+++ cypress/integration/userChangesLocation.spec.js
@@ -1,27 +1,9 @@
-// The timezone is hardcoded to America/Santarem in the npm scripts for
-// running cypress, and these coordinates are in Santarem.
-const position = {
-  coords: {
-    latitude: -2.44306,
-    longitude: -54.70833
-  }
-}
-
 describe('User on home page', () => {
   it('can change location with location select', () => {
-    const { latitude, longitude } = position.coords
-
     const now = new Date(1970, 0, 5, 10, 25, 15).getTime()
     const clock = cy.clock(now)
 
-    cy.visit(Cypress.env('HOST') || 'http://localhost:3000', {
-      onBeforeLoad(pageWindow) {
-        // stubbing getCurrentPosition to speed up test
-        cy
-          .stub(pageWindow.navigator.geolocation, 'getCurrentPosition')
-          .callsArgWith(0, position)
-      }
-    })
+    cy.visit(Cypress.env('HOST') || 'http://localhost:3000')
 
     cy.get('[data-test="location-select-container"]').click()
     cy.get('.Select-menu').scrollTo('top')
