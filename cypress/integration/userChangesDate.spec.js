c521231a3955c2dd98c28393997293c51ad9c1ff
Nathan Shane <nathan.shane@flocabulary.com>
2018-04-24 21:05:24 -0400

    Fixed user changes date test.

--- cypress/integration/userChangesDate.spec.js
+++ cypress/integration/userChangesDate.spec.js
@@ -1,6 +1,9 @@
+// IMPORTANT: This test will fail unless you start cypress in the npm scripts,
+// because it needs the timezone to be set by an env variable!
+
 describe('User on home page', () => {
   it('can change date with clock date picker', () => {
-    const now = new Date(1970, 0, 5, 10, 25, 15).getTime()
+    const now = new Date(2018, 3, 24, 10, 25, 15).getTime()
     const clock = cy.clock(now)
 
     cy.visit(Cypress.env('HOST') || 'http://localhost:3000', {
@@ -21,24 +24,28 @@ describe('User on home page', () => {
       }
     })
 
-    cy.contains('6:37 am')
-    cy.contains('6:53 pm')
+    cy.contains('Show My Location').click()
+
+    cy.contains('6:36 am')
+    cy.contains('6:39 pm')
     cy.contains('10:25 am')
     cy
       .get('[data-test="clock-date-select-container"] input')
-      .should('have.value', '1970-01-05')
+      .should('have.value', '2018-04-24')
 
     cy.contains('Date:').click()
     cy
       .get('.react-datepicker-popper')
-      .contains('20')
+      .contains('13')
       .click()
 
-    cy.contains('6:43 am')
-    cy.contains('6:58 pm')
+    cy.contains('6:38 am')
+    cy.contains('6:42 pm')
     cy.contains('10:25 am')
     cy
       .get('[data-test="clock-date-select-container"] input')
-      .should('have.value', '1970-01-20')
+      .should('have.value', '2018-04-13')
+
+    cy.location('search').should('eq', '?date=2018-04-13')
   })
 })
