130138e19c885c0a6d1fcd1488b62c1e10dab1c7
Nathan Shane <nathan.shane@flocabulary.com>
2018-05-01 21:02:13 -0400

    Added another speed test for speed query param.

--- cypress/integration/speed.js
+++ cypress/integration/speed.js
@@ -7,7 +7,21 @@ describe('Speed', () => {
 
     cy.visitWithStubbedLocation('/?location=Santar%25C3%25A9m_Brazil')
     cy.contains('10:25 am')
-    cy.tick(1020)
+    // this isn't just 1000 because it needs to be a multiple of `tickAmountMilliseconds`,
+    // or else the last tick won't get triggered
+    cy.tick(1005)
     cy.contains('10:26 am')
+  })
+
+  it('is multiplied by speed query param number', () => {
+    const now = new Date(2018, 0, 5, 10, 25, 0, 0).getTime()
+    const clock = cy.clock(now)
+
+    cy.visitWithStubbedLocation(
+      '/?location=Santar%25C3%25A9m_Brazil&speed=3600'
+    )
+    cy.contains('10:25 am')
+    cy.tick(1005)
+    cy.contains('11:25 am')
   })
 })
