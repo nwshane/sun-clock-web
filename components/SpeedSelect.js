5d465c7554f8ee95de79be5624c3b3a15e438589
Nathan Shane <nathan.shane@flocabulary.com>
2018-06-02 17:27:38 -0400

    Replaced rc-slider with rheostat.

--- components/SpeedSelect.js
+++ components/SpeedSelect.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import Slider from 'rc-slider/lib/Slider'
-import 'rc-slider/assets/index.css'
+import Router from 'next/router'
+import Rheostat from 'rheostat'
+import log10 from 'rheostat/lib/algorithms/log10'
+import 'rheostat/css/slider.css'
 
 import { setRateOfClockDateChange } from '~/data/actions'
+import { YEAR_CIRCLE_MIN_SPEED } from '~/data/constants'
+
+class PitComponent extends React.Component {
+  render() {
+    return <span className="year-clock-marker">Year</span>
+  }
+}
 
 class SpeedSelect extends React.Component {
-  handleChange = newSpeed => {
+  handleChange = (data, options = {}) => {
+    const newSpeed = data.values[0]
     this.props.dispatch(setRateOfClockDateChange(newSpeed))
+
+    if (options.pushHistory) {
+      Router.push({
+        pathname: Router.pathname,
+        query: Object.assign({}, Router.query, {
+          speed: newSpeed
+        })
+      })
+    }
   }
 
   render() {
@@ -15,7 +34,44 @@ class SpeedSelect extends React.Component {
 
     return (
       <div>
-        <Slider onChange={this.handleChange} value={rateOfClockDateChange} />
+        <p>Speed:</p>
+        <Rheostat
+          pitComponent={({ style, children }) => (
+            <div
+              style={{
+                ...style,
+                background: '#a2a2a2',
+                width: 2,
+                height: '15px',
+                zIndex: 99999,
+                transform: 'translate(0, -65%)'
+              }}
+            />
+          )}
+          algorithm={log10}
+          min={1}
+          max={10 ** 8}
+          onValuesUpdated={this.handleChange}
+          onChange={data => this.handleChange(data, { pushHistory: true })}
+          values={[rateOfClockDateChange]}
+          pitPoints={[YEAR_CIRCLE_MIN_SPEED]}
+        />
+        <style jsx>{``}</style>
+        <style global jsx>{`
+          .rheostat {
+            margin-top: 10px;
+          }
+          .rheostat-background {
+            background-color: #c6e8f4;
+            height: 3px;
+          }
+          .rheostat-handle {
+            border-radius: 50%;
+            width: 18px;
+            height: 18px;
+            transform: translate(0, -63%);
+          }
+        `}</style>
       </div>
     )
   }
