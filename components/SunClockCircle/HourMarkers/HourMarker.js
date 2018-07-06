7c3d0ed7de81da666b75370442e43c71077c921d
nwshane <django09@gmail.com>
2018-07-05 22:47:04 -0400

    Render HourMarker more efficiently

--- components/SunClockCircle/HourMarkers/HourMarker.js
+++ components/SunClockCircle/HourMarkers/HourMarker.js
@@ -4,38 +4,52 @@ import * as hourMarkerGetters from './getters'
 import { formatToHours } from '~/data/timeFormatters'
 import getDimensionFromBrowser from '~/data/getDimensionFromBrowser'
 
+const getFontFromBrowserDimension = dimension => dimension ** 0.05 * 17
+
+class _HourMarkerLine extends React.Component {
+  render() {
+    const { dispatch, ...otherProps } = this.props
+    return <line {...otherProps} />
+  }
+}
+
+const HourMarkerLine = connect((state, { time }) => ({
+  x1: hourMarkerGetters.getLineOuterX(state, time),
+  y1: hourMarkerGetters.getLineOuterY(state, time),
+  x2: hourMarkerGetters.getLineInnerX(state, time),
+  y2: hourMarkerGetters.getLineInnerY(state, time),
+  strokeWidth: 4,
+  stroke: 'black'
+}))(_HourMarkerLine)
+
+class _HourMarkerText extends React.Component {
+  render() {
+    const { dispatch, formattedDateText, ...otherProps } = this.props
+
+    return <text {...otherProps}>{formattedDateText}</text>
+  }
+}
+
+const HourMarkerText = connect((state, { time }) => ({
+  x: hourMarkerGetters.getTextX(state, time),
+  y: hourMarkerGetters.getTextY(state, time),
+  textAnchor: 'middle',
+  alignmentBaseline: 'central',
+  fontSize: `${getFontFromBrowserDimension(getDimensionFromBrowser())}px`,
+  formattedTimeText: formatToHours(time)
+}))(_HourMarkerText)
+
 class HourMarker extends React.Component {
   render() {
-    const { lineProps, textProps, formattedTimeText, showText } = this.props
+    const { time, showText } = this.props
 
     return (
       <g>
-        <line {...lineProps} />
-        {showText && <text {...textProps}>{formattedTimeText}</text>}
+        <HourMarkerLine {...{ time }} />
+        {showText && <HourMarkerText {...{ time }} />}
       </g>
     )
   }
 }
 
-const getFontFromBrowserDimension = dimension => dimension ** 0.05 * 17
-
-const mapStateToProps = (state, { time, showText }) => ({
-  lineProps: {
-    x1: hourMarkerGetters.getLineOuterX(state, time),
-    y1: hourMarkerGetters.getLineOuterY(state, time),
-    x2: hourMarkerGetters.getLineInnerX(state, time),
-    y2: hourMarkerGetters.getLineInnerY(state, time),
-    strokeWidth: 4,
-    stroke: 'black'
-  },
-  textProps: showText && {
-    x: hourMarkerGetters.getTextX(state, time),
-    y: hourMarkerGetters.getTextY(state, time),
-    textAnchor: 'middle',
-    alignmentBaseline: 'central',
-    fontSize: `${getFontFromBrowserDimension(getDimensionFromBrowser())}px`
-  },
-  formattedTimeText: formatToHours(time)
-})
-
-export default connect(mapStateToProps)(HourMarker)
+export default HourMarker
